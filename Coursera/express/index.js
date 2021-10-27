515eb7eedc63ee8c9dbfe774618a7f4726c3522d
Henry Le <h.le@datagumbo.com>
2021-10-27 18:37:32 -0500

    edit express index hs

--- Coursera/express/index.js
+++ Coursera/express/index.js
@@ -1,13 +1,27 @@
+// import dependencies
 const express = require('express');
 const http = require('http');
+const mg = require('morgan');
 
+// server parameters
 const hostName = 'localhost';
 const port = 3100;
 
+// create express app
 const app = express();
 
+// init morgan
+app.use(mg('dev'));
+
+// serve a static file from local storage
+app.use(express.static(__dirname + '/public'));
+
+// create express request handlers
 app.use((req, res, next)=>{
-    console.log("header :: ", req.headers);
+    // no need to log b/c morgan will serve enough logging info
+    // console.log("header :: ", req.headers);
+
+    // create a response header to client
     res.writeHead(200, {
         "Content-Type" : "text/html"
         , "Author": "Henry Le"
@@ -15,6 +29,7 @@ app.use((req, res, next)=>{
     res.end(`<html><body><p>this is a simple Express</p><html><body>`)
 })
 
+// fire up server and specify listening port
 const server = http.createServer(app);
 server.listen(port, hostName, () => {
     console.log(`Server is listening on http://${hostName}:${port}`);
