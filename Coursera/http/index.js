7d29f23b2d3b22be35203ce61c8599e64e43955f
Henry Le <henryle.b11@gmail.com>
2021-10-27 01:34:11 -0500

    fix file names

--- Coursera/http/index.js
+++ Coursera/http/index.js
@@ -1,51 +1,106 @@
+// import dependencies
+//  at the time I'm making this file ES6 for fs, path is in early beta test stage
+//  thus I chose to go with CommonJS  using 'require' instead of 'import'
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const http = require("http");
 
-const hostname = "localhost";
-const port = 1500;
+// define domain and port to listen to for this webapp
+const hostName = "localhost";
+const mainPort = "1500";
 
 const server = http.createServer((req, res) => {
-  console.log(`Request for ${req.url} by method ${req.method}`);
+  console.log(
+    `Requesting "${req.url}" with method "${req.method}"`
+  );
 
+  // check if method is get
   if (req.method == "GET") {
-    var fileUrl;
-    if (req.url == "/") fileUrl = "/index.html";
-    
-    else fileUrl = req.url;
+    var fileURL;
 
-    var filePath = path.resolve("./public" + fileUrl);
-    const fileExt = path.extname(filePath);
-    if (fileExt == ".html") {
-      fs.exists(filePath, (exists) => {
-        if (!exists) {
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html");
-          res.end(
-            `<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`
-          );
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        fs.createReadStream(filePath).pipe(res);
-      });
+    // build correct file path with homepage's path to be an exeption
+    if (req.url == "/") {
+      fileURL = "/index.html";
     } else {
-      res.statusCode = 404;
+      fileURL = req.url;
+    }
+
+    //build file path to local storage of file
+    var filePath = "./public" + fileURL;
+
+    // check if file req'ed is html format
+    var fileExt = path.extname(filePath);
+
+    //check if file type is in HTML format
+    if (fileExt == ".html") {
+
+
+    // ############### check if file exists ############
+    //  can do it by fs.access() or fs.stat()
+
+    // ------------------- with fs.access --------------------
+    //   try {
+    //     // NOTE: fs.exists are deprecated - it supports call back and async by default
+    //     // fs.access doesn't support call back and thus need to use with try and catch
+    //     // no need to wrap async ... await ... for fs.access  
+    //     fs.access(filePath, fs.constants.F_OK, (e) => {
+    //       res.setHeader("Content-Type", "text/html");
+    //       res.statusCode = 200;
+    //       fs.ReadStream(filePath).pipe(res);
+    //     });
+    //   } catch {
+    //     res.setHeader("Content-Type", "text/html");
+    //     res.statusCode = 404;
+    //     res.write(`<html><body><p>this is the catched error: ${e}</p>`);
+    //     res.end(
+    //       `<p>Error: ${res.statusCode} - Requested URL: ${fileURL} doesn't exist!</p><html><body>`
+    //     );
+    //     console.error("File doesn't exist");
+    //   }
+    // ------------------- end  fs.access --------------------
+
+
+    // ------------------- with fs.stat --------------------
+    // big advantage of fs.stat() is that it supports callback && async 
+  
+    fs.stat(filePath, (error, stats) => {
+        if (error) {
+            res.setHeader("Content-Type", "text/html");
+            res.statusCode = 404;
+            res.write(`<html><body><p>this is the catched error: ${e}</p>`);
+            res.end(
+            `<p>Error: ${res.statusCode} - Requested URL: ${fileURL} doesn't exist!</p><html><body>`
+            );
+            console.error("File doesn't exist :: ", error);
+        }
+        else {
+          console.log("Stats object for: " + filePath);
+          console.log(stats);
+        
+          // Using methods of the Stats object
+          console.log("Path is file:", stats.isFile());
+          console.log("Path is directory:", stats.isDirectory());
+
+          res.setHeader("Content-Type", "text/html");
+          res.statusCode = 200;
+          fs.ReadStream(filePath).pipe(res);
+
+        }
+      });
+    // ------------------- end with fs.stat --------------------
+    } else {
       res.setHeader("Content-Type", "text/html");
+      res.statusCode = 404;
       res.end(
-        `<html><body><h1>Error 404: ${fileUrl} not a HTML file</h1></body></html>`
+        `Error: ${res.statusCode} - Requested URL: ${fileURL} is not an HTML file!`
       );
     }
   } else {
-    res.statusCode = 404;
     res.setHeader("Content-Type", "text/html");
-    res.end(
-      `<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`
-    );
+    res.statusCode = 404;
+    res.end(`Error: Server doesn't accept method: ${req.method}`);
   }
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(mainPort, hostName, () =>
+  console.log(`Server starts at: ${hostName}:${mainPort}`)
+);
