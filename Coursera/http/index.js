507c2adfcec601da4b12a3000d5dbab781aabb58
Henry Le <h.le@datagumbo.com>
2021-10-26 19:47:02 -0500

    eod push

--- Coursera/http/index.js
+++ Coursera/http/index.js
@@ -1,6 +1,8 @@
 import http from 'http';
+import path from 'path'
 const hostname = 'localhost';
 const port = '3000';
+
 
 let chargeUnit = 2.3;
 let volume = 101.7;
@@ -10,7 +12,15 @@ var resultCalc = (c, v) => {
 }
 
 const server = http.createServer((req, res) => {
+    console.log("--------------------------------------");
     console.log("req headers", req.headers);
+    console.log(`request url ` + req.url + "method" + req.method);
+
+    if (req.method == "GET") {
+        if (req.url == '/') var fileURL = 'index.html';
+        else fileURL = req.url;
+    }
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
     res.end(`
